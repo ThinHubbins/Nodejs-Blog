9296130e5776276a3eb1bb7c08f20b7bbdc5177f
ThinHubbins <fredrickchinweze30@gmail.com>
2026-01-09 15:54:42 +0100

    Render Ready

--- Server/express.js
+++ Server/express.js
@@ -1,32 +1,41 @@
 const express = require("express");
 const morgan = require("morgan");
 const mongoose = require("mongoose");
+const path = require("path");
 const Blogpost = require("./models/blog");
-const { result } = require("lodash");
-require('dotenv').config();
-
+require("dotenv").config();
 
 const app = express();
-const dbURI = process.env.myURI   
 
-mongoose  
+// ✅ Environment variables
+const dbURI = process.env.MONGODB_URI;
+const PORT = process.env.PORT || 3000;
+
+// ✅ MongoDB connection
+mongoose
   .connect(dbURI)
-  .then((result) => {
-    app.listen(3000);
-    console.log("Connected");
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
+    });
+    console.log("MongoDB Connected");
   })
   .catch((err) => console.log(err));
 
+// ✅ View engine
 app.set("view engine", "ejs");
-app.set("views", "html");
+app.set("views", path.join(__dirname, "html"));
 
+// ✅ Middleware
 app.use(morgan("dev"));
-app.use(express.static("public"));
+app.use(express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded({ extended: true }));
 
+// ✅ Routes
 app.get("/", (req, res) => {
   res.redirect("/blogs");
 });
+
 app.get("/blogs", (req, res) => {
   Blogpost.find()
     .sort({ createdAt: -1 })
@@ -35,53 +44,41 @@ app.get("/blogs", (req, res) => {
     })
     .catch((err) => console.log(err));
 });
-app.get('/about', (req, res)=>{
-  res.render('about', { title: 'About'})
-})
+
+app.get("/about", (req, res) => {
+  res.render("about", { title: "About" });
+});
+
 app.get("/add-blog", (req, res) => {
   res.render("Add-Blog", { title: "Add-Blog" });
 });
-app.post('/blogs', (req, res)=>{
-  const postblogs = new Blogpost(req.body)
-  postblogs.save().then((result)=>{
-    res.redirect('/blogs')
-    
-  })
-})
-app.get('/blogs/:id', (req, res)=>{
+
+app.post("/blogs", (req, res) => {
+  const postblogs = new Blogpost(req.body);
+  postblogs.save().then(() => {
+    res.redirect("/blogs");
+  });
+});
+
+app.get("/blogs/:id", (req, res) => {
   const id = req.params.id;
-  Blogpost.findById(id).then((result)=>{
-    res.render('readmore', {title: 'Read', Blogs: result})
-    
-  }).catch(err => console.log(err)
-  )
-})
-app.delete('/blogs/:id', (req, res)=>{
-    const id = req.params.id;
-    Blogpost.findByIdAndDelete(id).then(result => {
-        res.json({ redirect: '/blogs' })
-    }).catch(err =>{
-        console.log(err);
-        
+  Blogpost.findById(id)
+    .then((result) => {
+      res.render("readmore", { title: "Read", Blogs: result });
     })
-})
+    .catch((err) => console.log(err));
+});
 
-// app.get("/blog", (req, res) => {
-//   const blogs = [
-//     {
-//       title: "Getting Started with JavaScript",
-//       author: "Kosi",
+app.delete("/blogs/:id", (req, res) => {
+  const id = req.params.id;
+  Blogpost.findByIdAndDelete(id)
+    .then(() => {
+      res.json({ redirect: "/blogs" });
+    })
+    .catch((err) => console.log(err));
+});
 
-//     },
-//     {
-//       title: "Understanding React Components",
-//       author: "Fredrick",
-//     },
-//   ];
-
-//   res.render("blog", { title: "blog", blogs });
-// });
-
+// ✅ 404 page
 app.use((req, res) => {
   res.status(404).render("error", { title: "404" });
 });
